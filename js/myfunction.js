4b902bfb5919f17917401152895b524dc1ece2dd
10dy10 <kdy93102@naver.com>
2022-05-17 14:22:34 +0900

    js 수정

--- js/myfunction.js
+++ js/myfunction.js
@@ -1,68 +1,66 @@
-$(function(){
-            
-    $(window).scroll(function(){
-        let scroll = $(window).scrollTop();
 
-        // 네비 css
-        if(scroll>1){$('.top-nav').css({
-            'border-bottom':'1px solid #000',
-            'background-color':'#fff',
-            'color':'#333',
-        })}else{$('.top-nav').css({
-            'border-bottom':'none',
-            'background-color':'transparent',
-            'color':'#fff',
-        })};
+window.addEventListener('scroll', function(){
+  let scroll = window.scrollY
 
+  console.log(scroll);
 
-        // 스킬 애니메이션 효과
-        if($(document).scrollTop()>400) {
-            $(".skill>li").each(function(e){
-                setTimeout(function(){
-                    $(".skill>li").eq(e).addClass("line");
-                }, e * 500 );
-            });
-        }
+  // navbar 색상 변경
+  if (scroll > 100) {
+    document.querySelector('.top-nav').classList.add('fixed')
+  } else {
+    document.querySelector('.top-nav').classList.remove('fixed')    
+  }
+
+  // 스킬 부분 스킬바 움직이게
+  const skills = document.querySelectorAll('.skill')
+  if( scroll > 1500) {
+    skills.forEach(function(e){
+      setTimeout(function(){
+        e.classList.add('line')
+      }, e * 500)
     })
-
-    // 포트폴리오 슬라이드
-    var swiper = new Swiper(".mySwiper", {
-        breakpoints:{
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-            768:{
-                slidesPerView: 2,
-                spaceBetween: 40,
-            },
-            1000:{
-                slidesPerView: 3,
-                spaceBetween: 10,
-            }
-        },
-        autoplay: {
-            delay: 3000,
-        },
-        loop: true,
-        pagination: {
-        el: ".swiper-pagination",
-        },
-    });
-
-    // 스무스 스크롤
-    var scroll = new SmoothScroll('a[href*="#"]',{
-        speed: 800,
-        easing: 'easeInOutCubic',
-    });
-
-    // AOS
-    AOS.init();
-
-    // index text 애니메이션 효과
-    let mainTxt = document.querySelector('.txt-box');
-    setTimeout(function(){
-        mainTxt.classList.add('active');
-    },1000)
-
+  }
 })
+
+
+$(function () {
+  // 포트폴리오 슬라이드
+  var swiper = new Swiper(".mySwiper", {
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  // 스무스 스크롤
+  // var scroll = new SmoothScroll('a[href*="#"]', {
+  //   speed: 800,
+  //   easing: "easeInOutCubic",
+  // });
+
+  // AOS
+  AOS.init();
+
+  // index text 애니메이션 효과
+  let mainTxt = document.querySelector(".txt-box");
+  setTimeout(function () {
+    mainTxt.classList.add("active");
+  }, 1000);
+});
